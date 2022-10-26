d3c7d13eff9d4ff2ae715c52c396e29dfe73da3c
João Lucas Gozzi <48017110+jlgozzi@users.noreply.github.com>
2022-10-26 10:58:43 -0400

    Initial commit

---
+++ src/data-source.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: ["src/entities/**/*.ts"],
+  migrations: ["src/migrations/**/*.ts"],
+});
+
+AppDataSource.initialize().catch((err) => {
+  console.error("Error during Data Source initialization", err);
+});
